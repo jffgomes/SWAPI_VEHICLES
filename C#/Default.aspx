f1fb02b83b39abf0a01fd3ba0a817409768daed8
jffgomes <52676060+jffgomes@users.noreply.github.com>
2019-08-26 14:32:10 -0300

    Add files via upload

---
+++ C#/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>SWAPI DEMO - Vehicles</title>
+        <link rel="stylesheet" href="css/Main.css"/>
+</head>
+<body>
+    <table style="margin-top: 80px">
+        <tr>
+            <td>
+                SWAPI - Demo
+            </td>
+            <td>
+                Recuperando dados da API Pública: <a href="https://swapi.co/api/vehicles/" target="_blank">SWAPI</a>
+            </td>
+        </tr>
+    </table>
+    <form id="form1" runat="server">
+            <div class="navbar">
+                <div id="logo">
+                    <p class="site-name"> <a class="navbar-brand" href="https://github.com/jffgomes/SWAPI_VEHICLES" target="_blank" title="GitHub" rel="home">GitHub</a></p> 
+                </div>
+                <a class="navbar-brand" href="https://swapi.co/" target="_blank" title="SWAPI" rel="home">SWAPI</a>  
+                <a style="float: right" class="navbar-brand" href="https://br.linkedin.com/in/jhonatan-gomes-50074b190" target="_blank" title="SWAPI" rel="home">LinkedIn</a>  <%--<a href="#news">News</a>--%>
+            
+            </div>
+        <div>
+
+            <div class="main" runat="server">
+    <div runat="server" id="dv_GridView" style="max-height:300px; overflow-y:scroll; overflow-x:hidden; margin-top:20px; font-family:Arial,Verdana,Tahoma;">
+        <asp:GridView  Visible="true" ID="gv_swapi_vehicles" CssClass="GridGis" Runat="server" DataKeyNames="NAME" AutoGenerateColumns="False" EnableModelValidation="True" HeaderStyle-CssClass="FixedHeader">
+        <Columns>
+            <asp:BoundField HeaderText="Name" DataField="NAME" SortExpression="NAME"></asp:BoundField>
+            <asp:BoundField HeaderText="Model" DataField="MODEL" SortExpression="MODEL"></asp:BoundField>
+            <asp:BoundField HeaderText="Price" DataField="cost_in_credits" SortExpression="cost_in_credits"></asp:BoundField>
+            <asp:BoundField HeaderText="Length" DataField="length" SortExpression="length"></asp:BoundField>         
+            <asp:BoundField HeaderText="Max Speed" DataField="max_atmosphering_speed" SortExpression="max_atmosphering_speed"></asp:BoundField>         
+            <asp:BoundField HeaderText="Crew" DataField="crew" SortExpression="crew"></asp:BoundField>         
+            <asp:BoundField HeaderText="Passengers" DataField="passengers" SortExpression="passengers"></asp:BoundField>         
+            <asp:BoundField HeaderText="Cargo" DataField="cargo_capacity" SortExpression="cargo_capacity"></asp:BoundField>         
+            <asp:BoundField HeaderText="Consumables" DataField="consumables" SortExpression="consumables"></asp:BoundField>         
+            <asp:BoundField HeaderText="Class" DataField="vehicle_class" SortExpression="vehicle_class"></asp:BoundField>         
+            <asp:BoundField HeaderText="url" DataField="url" SortExpression="url"></asp:BoundField>         
+        </Columns>
+    </asp:GridView>
+    </div>
+    <asp:Button OnCommand="getVehicles" CssClass="button" Text="Executar" runat="server" />
+               <asp:Label runat="server" ID="lb_error" Text=""/>
+</div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ C#/Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json.Linq;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    string SQL_S = Environment.GetEnvironmentVariable("SQL_S");
+    string SQL_U = Environment.GetEnvironmentVariable("SQL_U");
+    string SQL_P = Environment.GetEnvironmentVariable("SQL_P");
+    string SQL_DB = Environment.GetEnvironmentVariable("SQL_P");
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        gv_swapi_vehicles.Visible = false;
+    }
+
+    protected void getVehicles(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source="+ SQL_S + ";Initial Catalog="+ SQL_DB + ";Persist Security Info=True;User ID="+ SQL_U + ";Password="+ SQL_P);
+        SqlCommand cmd = new SqlCommand("SELECT NAME, MODEL, cost_in_credits, length, max_atmosphering_speed, crew, passengers, cargo_capacity, consumables, vehicle_class, url FROM SWAPI_VEHICLES", con);
+        string query = "";
+
+        JObject jObject = null;
+        JToken jentities = null;
+
+        try
+        {
+            jObject = JObject.Parse(call_swapi());
+            jentities = jObject["results"];
+
+            int count = 0;
+            foreach (JToken element in jentities)
+            {
+                query = @"IF NOT EXISTS (SELECT * FROM SWAPI_VEHICLES WHERE URL = '" + jentities[count]["url"].ToString() + @"')
+                        BEGIN
+                        ";
+                query += "INSERT INTO SWAPI_VEHICLES (NAME, MODEL, cost_in_credits, length, max_atmosphering_speed, crew, passengers, cargo_capacity, consumables, vehicle_class, url)";
+                query += " VALUES (@NAME, @MODEL, @cost_in_credits, @length, @max_atmosphering_speed, @crew, @passengers, @cargo_capacity, @consumables, @vehicle_class, @url)";
+
+                query = query.Replace("@NAME", "'" + jentities[count]["name"].ToString() + "'");
+                query = query.Replace("@MODEL", "'" + jentities[count]["model"].ToString() + "'");
+                query = query.Replace("@cost_in_credits", "'" + jentities[count]["cost_in_credits"].ToString() + "'");
+                query = query.Replace("@length", "'" + jentities[count]["length"].ToString() + "'");
+                query = query.Replace("@max_atmosphering_speed", "'" + jentities[count]["max_atmosphering_speed"].ToString() + "'");
+                query = query.Replace("@crew", "'" + jentities[count]["crew"].ToString() + "'");
+                query = query.Replace("@passengers", "'" + jentities[count]["passengers"].ToString() + "'");
+                query = query.Replace("@cargo_capacity", "'" + jentities[count]["cargo_capacity"].ToString() + "'");
+                query = query.Replace("@consumables", "'" + jentities[count]["consumables"].ToString() + "'");
+                query = query.Replace("@vehicle_class", "'" + jentities[count]["vehicle_class"].ToString() + "'");
+                query = query.Replace("@url", "'" + jentities[count]["url"].ToString() + "'");
+
+                query += " END;";
+                SqlCommand insert_CMD = new SqlCommand(query, con);
+                con.Open();
+                insert_CMD.ExecuteNonQuery();
+                con.Close();
+                count++;
+            }
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            gv_swapi_vehicles.DataSource = ds;
+            gv_swapi_vehicles.DataBind();
+            gv_swapi_vehicles.Visible = true;
+
+        }
+        catch (Exception)
+        {
+            lb_error.Text = "Error fetching data from source: <a href=\"https://swapi.co/api/vehicles/\"";
+        }  
+    }
+    
+    public string call_swapi()
+    {
+        string url = "https://swapi.co/api/vehicles/";
+        var client = new WebClient();
+        //client.Headers.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
+        var response = "";
+        try
+        {
+            response = client.DownloadString(url);
+        }
+        catch (Exception)
+        {
+            response = "Error in: " + url;
+        }
+        return response;
+    }
+}
